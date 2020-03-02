450b4ceccc039f2d78ace7e1aa4b15d9808789d9
nykitash05 <56485408+nykitash05@users.noreply.github.com>
2020-03-01 20:23:41 -0500

    Update train_phone_finder.py

--- train_phone_finder.py
+++ train_phone_finder.py
@@ -10,8 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+# Calculating performance of the model
 def calculatingaccuracy():
-    orgfile = glob.glob(sys.argv[1][2:]+'/*.txt')
+    orgfile = glob.glob(sys.argv[1][2:]+'/*.txt')                        # Append ~/ and read file name from command prompt
     filepath = "outputstats.txt"
     result = open("result.txt","w+")
     result.write("Filename\t" + "X\t" +"Y\t" + "Accepted/Not Accepted \n")
@@ -44,21 +45,21 @@ def calculatingaccuracy():
 
 def main():
     #Read image path
-    images = glob.glob(sys.argv[1][2:]+'/*.jpg')
+    images = glob.glob(sys.argv[1][2:]+'/*.jpg')                        # Append ~/ from command prompt
     
     # Looping through each image
     file = open("outputstats.txt","w+")
     file.write("Filename\t" + "X\t" +"Y\n")
-    for im in images:
-        with open(im, 'rb') as file:
+    for im in images:                                                   # Read each image in the folder
+        with open(im, 'rb') as file:                                    # Open Image to read
             image = Image.open(file)
             h,w = image.height,image.width
-            image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            ret,thresh = cv2.threshold(gray,67,250,0)
-            thresh = cv2.dilate(thresh, None)  # fill some holes
+            image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)    # Convert RGB to BGR
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)              # Convert RGB to GRAY
+            ret,thresh = cv2.threshold(gray,67,250,0)                   # To filter phone image from background of tiles/floor
+            thresh = cv2.dilate(thresh, None)                           # Fill some holes
             thresh = cv2.dilate(thresh, None)
-            thresh = cv2.erode(thresh, None)   # dilate made our shape larger, revert that
+            thresh = cv2.erode(thresh, None)                            # Dilate made our shape larger, revert that
             thresh = cv2.erode(thresh, None)
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
             rc = cv2.minAreaRect(contours[0])
@@ -67,10 +68,10 @@ def main():
             cv2.drawContours(image,[box],0,(0,255,0),2)
             centre = [rc[0][0]/w, rc[0][1]/h]
             filename = im.split('\\')[1]
-            plt.imsave ('outputimage/'+ filename , image)
+            plt.imsave ('outputimage/'+ filename , image)               # Drawing image with bounding box
             file = open("outputstats.txt","a")
             file.write(filename + '\t' + str(math.ceil(centre[0] * 10000.0) / 10000.0) + '\t' + str(math.ceil(centre[1] * 10000.0) / 10000.0) + '\n')
     file.close()
     
-    calculatingaccuracy()
+    calculatingaccuracy()                                               # Calling performance calculator function
 main()
