fc4a2e85b9c318553443cef7cea19bb5cd877d8e
nykitash05 <56485408+nykitash05@users.noreply.github.com>
2020-03-01 20:32:29 -0500

    Update find_phone.py

--- find_phone.py
+++ find_phone.py
@@ -13,20 +13,21 @@ import math
 
 def main():
     #Read image path for sysin
-    images = glob.glob(sys.argv[1][2:])                          # Append ~/ and read file name from command prompt
-    with open(images, 'rb') as file:                                 #Open Image to read
-        image = Image.open(file)
-        h,w = image.height,image.width
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Convert RGB to BGR
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)           # Convert RGB to GRAY
-        ret,thresh = cv2.threshold(gray,67,250,0)                # To filter phone image from background of tiles/floor
-        thresh = cv2.dilate(thresh, None)                        # fill some holes
-        thresh = cv2.dilate(thresh, None)
-        thresh = cv2.erode(thresh, None)                         # dilate made our shape larger, revert that
-        thresh = cv2.erode(thresh, None)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        rc = cv2.minAreaRect(contours[0])
-        centre = [rc[0][0]/w, rc[0][1]/h]
-        sys.stdout.write(str(round(centre[0] ,4)) + "  " + str(round(centre[1] ,4)))
+    images = glob.glob(sys.argv[1][2:])                              # Append ~/ and read file name from command prompt
+    for im in images:
+        with open(im, 'rb') as file:                                 # Open Image to read
+            image = Image.open(file)
+            h,w = image.height,image.width
+            image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) # Convert RGB to BGR
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)           # Convert RGB to GRAY
+            ret,thresh = cv2.threshold(gray,67,250,0)                # To filter phone image from background of tiles/floor
+            thresh = cv2.dilate(thresh, None)                        # fill some holes
+            thresh = cv2.dilate(thresh, None)
+            thresh = cv2.erode(thresh, None)                         # dilate made our shape larger, revert that
+            thresh = cv2.erode(thresh, None)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            rc = cv2.minAreaRect(contours[0])
+            centre = [rc[0][0]/w, rc[0][1]/h]
+            sys.stdout.write(str(round(centre[0] ,4)) + "  " + str(round(centre[1] ,4)))
   
 main()
